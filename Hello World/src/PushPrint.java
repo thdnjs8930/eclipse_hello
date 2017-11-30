95cbe682ef5ae47b44a1328916f9ed83a84df95c
K SY <thdnjs8930@daum.net>
2017-11-30 15:11:44 +0900

    modified on local repository

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -4,6 +4,8 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java Class file Push");
+		System.out.println("Hello world Hi!");
+		
 	}
 
 }
