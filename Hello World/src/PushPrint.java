413d4a87a6069d1e668812140fda9cabae9044aa
thdnjs8930 <34118313+thdnjs8930@users.noreply.github.com>
2017-11-30 15:09:27 +0900

    modified on github

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -4,6 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java Class file Push");
+		System.out.println("Hello world 365");
 	}
 
 }
